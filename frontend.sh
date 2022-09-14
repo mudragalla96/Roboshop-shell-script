7a59b22d247d9cfef64e954ad870ed16f0599928
vykunta <mudragalla96@gmail.com>
2022-09-14 16:24:13 +0530

    add exit status to frontend

--- frontend.sh
+++ frontend.sh
@@ -1,19 +1,21 @@
  echo installing nginx
  yum install nginx -y &>>/tmp/frontend
+ echo status = $?
 
 echo downloading the nginx web content
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 cd /usr/share/nginx/html
-
+echo status = $?
 echo removing the old content
 rm -rf *
 
 echo extracting web content
 unzip /tmp/frontend.zip &>>/tmp/frontend
-
+echo status = $?
 mv frontend-main/static/* .
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
-
+echo status = $?
 echo starting nginx services
 systemctl enable nginx &>>/tmp/frontend
 systemctl restart nginx &>>/tmp/frontend
+echo status = $?
