ae77d106c4c13ad9651f4d6ca75a7dbb29c437bc
vykunta <mudragalla96@gmail.com>
2022-09-16 23:16:07 +0530

    add script for mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -2,6 +2,7 @@ LOG_FILE=/tmp/mongodb
 echo "setting Mongodb Repo"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG_FILE
 echo status = $?
+
 echo "install Mongodb server"
 yum install -y mongodb-org &>>$LOG_FILE
 echo status = $?
