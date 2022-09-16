2f32c6f8d6392bef85662e81e7f4afaa566aa924
vykunta <mudragalla96@gmail.com>
2022-09-16 23:13:51 +0530

    special variables

---
+++ mongodb.sh
@@ -0,0 +1,11 @@
+LOG_FILE=/tmp/mongodb
+echo "setting Mongodb Repo"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG_FILE
+echo status = $?
+echo "install Mongodb server"
+yum install -y mongodb-org &>>$LOG_FILE
+echo status = $?
+echo "starting Mongodb service"
+systemctl enable mongod &>>$LOG_FILE
+systemctl restart mongod &>>$LOG_FILE
+echo status = $?
