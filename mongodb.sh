8c24e2bb059f297403e0a2c35009f16e22e75988
vykunta <mudragalla96@gmail.com>
2022-09-16 23:51:13 +0530

    add script for mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -6,6 +6,10 @@ echo status = $?
 echo "install Mongodb server"
 yum install -y mongodb-org &>>$LOG_FILE
 echo status = $?
+
+echo 'upadte MongoDB Listen Address'
+sed  -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+echo status = $?
 echo "starting Mongodb service"
 systemctl enable mongod &>>$LOG_FILE
 systemctl restart mongod &>>$LOG_FILE
