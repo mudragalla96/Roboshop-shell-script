fe99430f9750c65566f28e08060afd7fbae3fc27
vykunta <mudragalla96@gmail.com>
2022-09-19 10:52:34 +0530

    add if conditions to catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -2,40 +2,86 @@ LOG_FILE=/tmp/catalogue
 
 echo "Setup NodeJS Repo File"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG_FILE}
-echo status = $?
+if [ $? -eq 0 ]; then
+  echo status = SUCCESS
+else
+  echo status = FAILURE
+  exit 1
+fi
 
 echo "Installing NodeJS Server"
 yum install nodejs -y &>>${LOG_FILE}
-echo status = $?
+if [ $? -eq 0 ]; then
+  echo status = SUCCESS
+else
+  echo status = FAILURE
+  exit 1
+fi
+
 
 echo "Add Roboshop Application User"
 useradd roboshop &>>${LOG_FILE}
-echo status = $?
+if [ $? -eq 0 ]; then
+  echo status = SUCCESS
+else
+  echo status = FAILURE
+  exit 1
+fi
+
 
 echo "Download Catalogue Application Code"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>${LOG_FILE}
-echo status = $?
+if [ $? -eq 0 ]; then
+  echo status = SUCCESS
+else
+  echo status = FAILURE
+  exit 1
+fi
+
 
 cd /home/roboshop
 
 echo "Extract Catalogue Application Code"
 unzip /tmp/catalogue.zip &>>${LOG_FILE}
-echo status = $?
+if [ $? -eq 0 ]; then
+  echo status = SUCCESS
+else
+  echo status = FAILURE
+  exit 1
+fi
+
 
 mv catalogue-main catalogue
 cd /home/roboshop/catalogue
 
 echo "Install NodeJS Dependencies"
 npm install &>>${LOG_FILE}
-echo status = $?
+if [ $? -eq 0 ]; then
+  echo status = SUCCESS
+else
+  echo status = FAILURE
+  exit 1
+fi
+
 
 echo "Setup Catalogue Service"
 mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>${LOG_FILE}
-echo status = $?
+if [ $? -eq 0 ]; then
+  echo status = SUCCESS
+else
+  echo status = FAILURE
+  exit 1
+fi
+
 
 systemctl daemon-reload &>>${LOG_FILE}
 systemctl enable catalogue &>>${LOG_FILE}
 
 echo "Starting Catalogue Service"
 systemctl start catalogue &>>${LOG_FILE}
-echo status = $?
+if [ $? -eq 0 ]; then
+  echo status = SUCCESS
+else
+  echo status = FAILURE
+  exit 1
+fi
