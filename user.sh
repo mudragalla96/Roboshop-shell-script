666057c3ef557db4eef06fb882de21dc0a10cea2
vykunta <mudragalla96@gmail.com>
2022-09-19 21:24:11 +0530

    add user script for roboshop

--- user.sh
+++ user.sh
@@ -1,55 +1,6 @@
 LOG_FILE=/tmp/user
 
 source common.sh
+COMPONENT=user
 
-echo "Setup NodeJS Repo File"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG_FILE}
-StatusCheck $?
-
-echo "Installing NodeJS Server"
-yum install nodejs -y &>>${LOG_FILE}
-StatusCheck $?
-
-id roboshop &>>${LOG_FILE}
-if [ $? -ne 0 ]; then
- echo "Add Roboshop Application User"
- useradd roboshop &>>${LOG_FILE}
- StatusCheck $?
-
-fi
-echo "Download User Application Code"
-curl -s -L -o /tmp/user.zip "https://github.com/roboshop-devops-project/user/archive/main.zip" &>>${LOG_FILE}
-StatusCheck $?
-
-
-cd /home/roboshop
-
-echo "Clean Old App Content"
-rm -rf user &>>${LOG_FILE}
-StatusCheck $?
-
-echo "Extract Catalogue Application Code"
-unzip /tmp/user.zip &>>${LOG_FILE}
-StatusCheck $?
-
-mv user-main user
-cd /home/roboshop/catalogue
-
-echo "Install NodeJS Dependencies"
-npm install &>>${LOG_FILE}
-StatusCheck $?
-
-echo "Update SystemD Service File"
-sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' /home/roboshop/user/systemd.service
-StatusCheck $?
-
-echo "Setup User Service"
-mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service &>>${LOG_FILE}
-StatusCheck $?
-
-systemctl daemon-reload &>>${LOG_FILE}
-systemctl enable user &>>${LOG_FILE}
-
-echo "Start User Service"
-systemctl start user &>>${LOG_FILE}
-StatusCheck $?
+NodeJS
