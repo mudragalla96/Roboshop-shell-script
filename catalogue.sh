7a88cb16f353b78b47250a74f1e1c9b9c26d3318
vykunta <mudragalla96@gmail.com>
2022-09-19 13:08:02 +0530

    add color print to catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -19,30 +19,32 @@ fi
 echo "Installing NodeJS Server"
 yum install nodejs -y &>>${LOG_FILE}
 if [ $? -eq 0 ]; then
-  echo status = SUCCESS
+  echo -e status = "\e[32mSUCCESS\e[0m"
 else
-  echo status = FAILURE
+  echo -e status = "\e[31mFAILURE\e0m"
   exit 1
 fi
+
 
 id roboshop &>>${LOG_FILE}
 if [ $? -ne 0 ]; then
  echo "Add Roboshop Application User"
  useradd roboshop &>>${LOG_FILE}
  if [ $? -eq 0 ]; then
-   echo status = SUCCESS
+   echo -e status = "\e[32mSUCCESS\e[0m"
  else
-  echo status = FAILURE
-  exit 1
+   echo -e status = "\e[31mFAILURE\e0m"
+   exit 1
  fi
+
 fi
 
 echo "Download Catalogue Application Code"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>${LOG_FILE}
 if [ $? -eq 0 ]; then
-  echo status = SUCCESS
+  echo -e status = "\e[32mSUCCESS\e[0m"
 else
-  echo status = FAILURE
+  echo -e status = "\e[31mFAILURE\e0m"
   exit 1
 fi
 
@@ -52,20 +54,22 @@ cd /home/roboshop
 echo "Clean Old App Content"
 rm -rf catalogue &>>${LOG_FILE}
 if [ $? -eq 0 ]; then
-  echo status = SUCCESS
+  echo -e status = "\e[32mSUCCESS\e[0m"
 else
-  echo status = FAILURE
+  echo -e status = "\e[31mFAILURE\e0m"
   exit 1
 fi
+
 
 echo "Extract Catalogue Application Code"
 unzip /tmp/catalogue.zip &>>${LOG_FILE}
 if [ $? -eq 0 ]; then
-  echo status = SUCCESS
+  echo -e status = "\e[32mSUCCESS\e[0m"
 else
-  echo status = FAILURE
+  echo -e status = "\e[31mFAILURE\e0m"
   exit 1
 fi
+
 
 
 mv catalogue-main catalogue
@@ -73,20 +77,21 @@ cd /home/roboshop/catalogue
 
 echo "Install NodeJS Dependencies"
 npm install &>>${LOG_FILE}
-if [ $? -eq 0 ]; then
-  echo status = SUCCESS
-else
-  echo status = FAILURE
-  exit 1
-fi
+iif [ $? -eq 0 ]; then
+   echo -e status = "\e[32mSUCCESS\e[0m"
+ else
+   echo -e status = "\e[31mFAILURE\e0m"
+   exit 1
+ fi
+
 
 
 echo "Setup Catalogue Service"
 mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>${LOG_FILE}
 if [ $? -eq 0 ]; then
-  echo status = SUCCESS
+  echo -e status = "\e[32mSUCCESS\e[0m"
 else
-  echo status = FAILURE
+  echo -e status = "\e[31mFAILURE\e0m"
   exit 1
 fi
 
@@ -97,8 +102,9 @@ systemctl enable catalogue &>>${LOG_FILE}
 echo "Starting Catalogue Service"
 systemctl start catalogue &>>${LOG_FILE}
 if [ $? -eq 0 ]; then
-  echo status = SUCCESS
+  echo -e status = "\e[32mSUCCESS\e[0m"
 else
-  echo status = FAILURE
+  echo -e status = "\e[31mFAILURE\e0m"
   exit 1
 fi
+
