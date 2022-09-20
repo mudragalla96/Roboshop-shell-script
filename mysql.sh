598e68f49381d1cbd5085f1e5bc5ce2785746298
vykunta <mudragalla96@gmail.com>
2022-09-20 10:49:24 +0530

    add mysql script for roboshop

--- mysql.sh
+++ mysql.sh
@@ -22,6 +22,9 @@ StatusCheck $?
  echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${ROBOSHOP_MYSQL_PASSWORD}');
        FLUSH PRIVILEGES;" >/tmp/root-pass.sql
 
+ echo "Change the default root password"
+ mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}" >/tmp/root-pass.sql &>>${LOG_FILE}
+ StatusCheck $?
 
  #mysql_secure_installation
 
