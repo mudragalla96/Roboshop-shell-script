a908519947a9107416ec4a261295de9d5627e351
vykunta <mudragalla96@gmail.com>
2022-09-21 10:38:44 +0530

    add rabbitmq script for roboshop

---
+++ rabbitmq.sh
@@ -0,0 +1,28 @@
+COMPONENT=rabbitmq
+source common.sh
+LOG_FILE=/tmp/${COMPONENT}
+
+echo "Install RabbitMQ Repos"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>$LOG_FILE
+StatusCheck $?
+
+echo "Install Erlang & RabbitMQ"
+yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm rabbitmq-server -y
+StatusCheck $?
+
+echo "Start RabbitMQ Server"
+ systemctl enable rabbitmq-server &>>$LOG_FILE
+ systemctl restart rabbitmq-server &>>$LOG_FILE
+StatusCheck $?
+
+echo "Add Application User RabbitMQ"
+rabbitmqctl add_user roboshop roboshop123 &>>$LOG_FILE
+StatusCheck $?
+
+echo "Add Application User Tags in RabbitMQ "
+ rabbitmqctl set_user_tags roboshop administrator &>>$LOG_FILE
+StatusCheck $?
+
+ echo "Add Permissions for App user in RabbitMQ"
+ rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOG_FILE
+ StatusCheck $?
