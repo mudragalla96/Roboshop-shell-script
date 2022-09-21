5479adefe3ea85fe0dc3b3f2f0a1e8664ef1590f
vykunta <mudragalla96@gmail.com>
2022-09-21 15:34:30 +0530

    add roboshop config file to frontend

--- mysql.sh
+++ mysql.sh
@@ -1,5 +1,10 @@
 LOG_FILE=/tmp/mysql
 source common.sh
+
+if [ -z "${ROBOSHOP_MYSQL_PASSWORD}" ]; then
+  echo "\e[31m ROBOSHOP_MYSQL_PASSWORD env variable is needed\e[0m"
+  exit 1
+fi
 echo "Setup MySQL Repo"
  curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>${LOG_FILE}
  StatusCheck $?
